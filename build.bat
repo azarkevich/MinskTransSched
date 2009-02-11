92719fa1a9eb549f33f3514a26d4bd0f698afe0a
azarkevich <azarkevich@1edd68fb-5641-41d5-9a6f-76477202bf19>
2009-02-11 17:15:09 +0000

    add: root build system based on ant release 1.4.52
    git-svn-id: https://minsktranssched.svn.sourceforge.net/svnroot/minsktranssched/trunk@17 1edd68fb-5641-41d5-9a6f-76477202bf19

--- build.bat
+++ build.bat
@@ -1,9 +1,2 @@
-cd MinskTransSched
 call %ANT_HOME%\bin\ant.bat clean
-call %ANT_HOME%\bin\ant.bat jar
-
-cd ..
-c:\cygwin\bin\bash build.sh
-
-cd MinskTransSched
-call %ANT_HOME%\bin\ant.bat clean
+call %ANT_HOME%\bin\ant.bat build
