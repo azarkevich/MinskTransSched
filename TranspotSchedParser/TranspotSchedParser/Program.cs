94258d00e79ff1c1b491966c2f1e0847e2810f8d
Sergey Azarkevich <azarkevich@gmail.com>
2015-07-16 10:09:36 +0300

    Parser

--- TranspotSchedParser/TranspotSchedParser/Program.cs
+++ TranspotSchedParser/TranspotSchedParser/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace TranspotSchedParser
 {
@@ -6,7 +7,32 @@ namespace TranspotSchedParser
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			var routesLines = System.IO.File.ReadAllLines(@"/home/sazarkevich/work.d/MinskTransSched/get-data/.tmp/routes.txt");
+
+			var header = routesLines[0].Split(';');
+
+			var indexes4Extract = new int[] {
+				Array.IndexOf(header, "RouteNum"),
+				Array.IndexOf(header, "RouteName"),
+			};
+
+
+			List<string> route = new List<string>();
+
+			for (int i = 1; i < routesLines.Length; i++) {
+				var routeLine = routesLines[i].Split(';');
+
+				route.Clear();
+
+				for (int j = 0; j < indexes4Extract.Length; j++) {
+					route.Add(routeLine[indexes4Extract[j]]);
+				}
+
+				for (int j = 0; j < route.Count; j++) {
+					Console.Write(route[j]);
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
